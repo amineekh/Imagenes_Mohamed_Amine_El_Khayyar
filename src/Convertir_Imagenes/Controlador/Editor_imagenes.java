d8c9cc8ddf0003ff15386fce1ee86e0686bd692f
amine <amineelkhayyar8@gmail.com>
2024-02-13 18:55:26 +0100

    13 - 02 - 2024
    Trabajooo Hechoooooo falta limar detalles

--- src/Convertir_Imagenes/Controlador/Editor_imagenes.java
+++ src/Convertir_Imagenes/Controlador/Editor_imagenes.java
@@ -1,11 +1,15 @@
 package Convertir_Imagenes.Controlador;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
-public class Editor_imagenes {
+public class Editor_imagenes extends Component {
     private JPanel Mainpanel;
     private JMenuBar menubar;
     private JMenu menu_archivo;
@@ -39,6 +43,28 @@ public class Editor_imagenes {
     private JSlider slider5;
     private JSlider slider6;
     private JSlider slider7;
+    private JLabel label_destino_img;
+    private JLabel label_parametro_1;
+    private JLabel label_1pr_100;
+    private JLabel label_1pr_0;
+    private JLabel label_parametro_2;
+    private JLabel label_2pr_0;
+    private JLabel label_2pr_100;
+    private JLabel label_parametro_3;
+    private JLabel label_3pr_0;
+    private JLabel label_3pr_100;
+    private JLabel label_parametro_4;
+    private JLabel label_4pr_100;
+    private JLabel label_4pr_0;
+    private JLabel label_parametro_5;
+    private JLabel label_5pr_0;
+    private JLabel label_5pr_100;
+    private JLabel label_parametro_6;
+    private JLabel label_6pr_100;
+    private JLabel label_6pr_0;
+    private JLabel label_parametro_7;
+    private JLabel label_7pr_0;
+    private JLabel label_7pr_100;
 
     public Editor_imagenes() {
         // Agregar ActionListener al item_abrir
@@ -52,7 +78,30 @@ public class Editor_imagenes {
         item_guardar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                try{
+                    ImageIcon icon = (ImageIcon) label_destino_img.getIcon();
+                    Image image = icon.getImage();
+                    BufferedImage bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB);
+                    Graphics2D g2d=bufferedImage.createGraphics();
+                    g2d.drawImage(image, 0, 0, null);
+                    g2d.dispose();
+                    JFileChooser fileChooser = new JFileChooser();
+                    int returnValue = fileChooser.showOpenDialog(null);
 
+                    if (returnValue == JFileChooser.APPROVE_OPTION) {
+                        JOptionPane.showMessageDialog(null, "Archivo seleccionado: " + fileChooser.getSelectedFile().getAbsolutePath());
+                    }
+                    String ruta_a_guardar=fileChooser.getSelectedFile().getPath();
+                    File outputFile = new File(ruta_a_guardar);
+                    ImageIO.write(bufferedImage,"jpg",outputFile);
+                    JOptionPane.showMessageDialog(Editor_imagenes.this,"La imagen ha sido guardada con éxito.");
+
+                }catch (IOException ex){
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(Editor_imagenes.this, "Fallo al guardar la imagen");
+
+
+                }
             }
         });
 
@@ -60,92 +109,399 @@ public class Editor_imagenes {
         item_cerrar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                System.exit(0);
             }
         });
 
 
-
-
-        boton_convertir.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
         item_blanco_negro.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                String ruta_img_blanco_negro= "src/Convertir_Imagenes/img/Costa_blanco_negro.jpg";
+                ImageIcon img_icon = new ImageIcon(ruta_img_blanco_negro);
+                label_destino_img.setIcon(img_icon);
+                label_destino_img.setVisible(false);
+
+                label_parametro_1.setVisible(true);
+                slider1.setVisible(true);
+                label_1pr_0.setVisible(true);
+                label_1pr_100.setVisible(true);
+
+                label_parametro_2.setVisible(false);
+                label_parametro_3.setVisible(false);
+                label_parametro_4.setVisible(false);
+                label_parametro_5.setVisible(false);
+                label_parametro_6.setVisible(false);
+                label_parametro_7.setVisible(false);
+
+                label_2pr_0.setVisible(false);
+                label_3pr_0.setVisible(false);
+                label_4pr_0.setVisible(false);
+                label_5pr_0.setVisible(false);
+                label_6pr_0.setVisible(false);
+                label_7pr_0.setVisible(false);
+
+                label_2pr_100.setVisible(false);
+                label_3pr_100.setVisible(false);
+                label_4pr_100.setVisible(false);
+                label_5pr_100.setVisible(false);
+                label_6pr_100.setVisible(false);
+                label_7pr_100.setVisible(false);
+
+                slider2.setVisible(false);
+                slider3.setVisible(false);
+                slider4.setVisible(false);
+                slider5.setVisible(false);
+                slider6.setVisible(false);
+                slider7.setVisible(false);
+
+                boton_convertir.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        label_destino_img.setVisible(true);
+                    }
+                });
+
             }
         });
+
+
         item_grises.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String ruta_img_blanco_negro= "src/Convertir_Imagenes/img/Costa_tono_grises.jpg";
+                ImageIcon img_icon = new ImageIcon(ruta_img_blanco_negro);
+                label_destino_img.setIcon(img_icon);
+                label_destino_img.setVisible(false);
 
+                label_parametro_1.setVisible(true);
+                label_1pr_0.setVisible(true);
+                label_1pr_100.setVisible(true);
+                slider1.setVisible(true);
+
+                label_2pr_100.setVisible(true);
+                label_parametro_2.setVisible(true);
+                label_2pr_0.setVisible(true);
+                slider2.setVisible(true);
+
+                label_parametro_3.setVisible(false);
+                label_parametro_4.setVisible(false);
+                label_parametro_5.setVisible(false);
+                label_parametro_6.setVisible(false);
+                label_parametro_7.setVisible(false);
+
+                label_3pr_0.setVisible(false);
+                label_4pr_0.setVisible(false);
+                label_5pr_0.setVisible(false);
+                label_6pr_0.setVisible(false);
+                label_7pr_0.setVisible(false);
+
+                label_3pr_100.setVisible(false);
+                label_4pr_100.setVisible(false);
+                label_5pr_100.setVisible(false);
+                label_6pr_100.setVisible(false);
+                label_7pr_100.setVisible(false);
+
+                slider3.setVisible(false);
+                slider4.setVisible(false);
+                slider5.setVisible(false);
+                slider6.setVisible(false);
+                slider7.setVisible(false);
+
+                boton_convertir.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        label_destino_img.setVisible(true);
+                    }
+                });
             }
         });
         item_Sepia.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String ruta_img_blanco_negro= "src/Convertir_Imagenes/img/Costa_sepia.jpg";
+                ImageIcon img_icon = new ImageIcon(ruta_img_blanco_negro);
+                label_destino_img.setIcon(img_icon);
+                label_destino_img.setVisible(false);
 
+                label_parametro_1.setVisible(true);
+                label_1pr_0.setVisible(true);
+                label_1pr_100.setVisible(true);
+                slider1.setVisible(true);
+
+                label_2pr_100.setVisible(true);
+                label_parametro_2.setVisible(true);
+                label_2pr_0.setVisible(true);
+                slider2.setVisible(true);
+
+                label_parametro_3.setVisible(true);
+                label_3pr_0.setVisible(true);
+                label_3pr_100.setVisible(true);
+                slider3.setVisible(true);
+
+                label_parametro_4.setVisible(false);
+                label_parametro_5.setVisible(false);
+                label_parametro_6.setVisible(false);
+                label_parametro_7.setVisible(false);
+
+                label_4pr_0.setVisible(false);
+                label_5pr_0.setVisible(false);
+                label_6pr_0.setVisible(false);
+                label_7pr_0.setVisible(false);
+
+                label_4pr_100.setVisible(false);
+                label_5pr_100.setVisible(false);
+                label_6pr_100.setVisible(false);
+                label_7pr_100.setVisible(false);
+
+
+                slider4.setVisible(false);
+                slider5.setVisible(false);
+                slider6.setVisible(false);
+                slider7.setVisible(false);
+
+                boton_convertir.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        label_destino_img.setVisible(true);
+                    }
+                });
             }
         });
         item_altocontraste.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String ruta_img_blanco_negro= "src/Convertir_Imagenes/img/Costa_alto_contraste.jpg";
+                ImageIcon img_icon = new ImageIcon(ruta_img_blanco_negro);
+                label_destino_img.setIcon(img_icon);
+                label_destino_img.setVisible(false);
+
+                label_parametro_1.setVisible(true);
+                label_1pr_0.setVisible(true);
+                label_1pr_100.setVisible(true);
+                slider1.setVisible(true);
+
+                label_2pr_100.setVisible(true);
+                label_parametro_2.setVisible(true);
+                label_2pr_0.setVisible(true);
+                slider2.setVisible(true);
+
+                label_parametro_3.setVisible(true);
+                label_3pr_0.setVisible(true);
+                label_3pr_100.setVisible(true);
+                slider3.setVisible(true);
+
+                label_parametro_4.setVisible(true);
+                label_4pr_0.setVisible(true);
+                label_4pr_100.setVisible(true);
+                slider4.setVisible(true);
+
+                label_parametro_5.setVisible(false);
+                label_parametro_6.setVisible(false);
+                label_parametro_7.setVisible(false);
+
+                label_5pr_0.setVisible(false);
+                label_6pr_0.setVisible(false);
+                label_7pr_0.setVisible(false);
+
+                label_5pr_100.setVisible(false);
+                label_6pr_100.setVisible(false);
+                label_7pr_100.setVisible(false);
+
+                slider5.setVisible(false);
+                slider6.setVisible(false);
+                slider7.setVisible(false);
+
+                boton_convertir.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        label_destino_img.setVisible(true);
+                    }
+                });
 
             }
         });
         item_pixelada.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String ruta_img_blanco_negro= "src/Convertir_Imagenes/img/Costa_pixelada.jpg";
+                ImageIcon img_icon = new ImageIcon(ruta_img_blanco_negro);
+                label_destino_img.setIcon(img_icon);
+                label_destino_img.setVisible(false);
+
+                label_parametro_1.setVisible(true);
+                label_1pr_0.setVisible(true);
+                label_1pr_100.setVisible(true);
+                slider1.setVisible(true);
+
+                label_2pr_100.setVisible(true);
+                label_parametro_2.setVisible(true);
+                label_2pr_0.setVisible(true);
+                slider2.setVisible(true);
+
+                label_parametro_3.setVisible(true);
+                label_3pr_0.setVisible(true);
+                label_3pr_100.setVisible(true);
+                slider3.setVisible(true);
+
+                label_parametro_4.setVisible(true);
+                label_4pr_0.setVisible(true);
+                label_4pr_100.setVisible(true);
+                slider4.setVisible(true);
+
+                label_parametro_5.setVisible(true);
+                label_5pr_0.setVisible(true);
+                label_5pr_100.setVisible(true);
+                slider5.setVisible(true);
+
+                label_parametro_6.setVisible(false);
+                label_parametro_7.setVisible(false);
+
+                label_6pr_0.setVisible(false);
+                label_7pr_0.setVisible(false);
+
+
+                label_6pr_100.setVisible(false);
+                label_7pr_100.setVisible(false);
+
+
+                slider6.setVisible(false);
+                slider7.setVisible(false);
+
+                boton_convertir.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        label_destino_img.setVisible(true);
+                    }
+                });
 
             }
         });
         item_crearicono.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String ruta_img_blanco_negro= "src/Convertir_Imagenes/img/Costa.ico";
+                ImageIcon img_icon = new ImageIcon(ruta_img_blanco_negro);
+                label_destino_img.setIcon(img_icon);
+                label_destino_img.setVisible(false);
+
+                label_parametro_1.setVisible(true);
+                label_1pr_0.setVisible(true);
+                label_1pr_100.setVisible(true);
+                slider1.setVisible(true);
+
+                label_2pr_100.setVisible(true);
+                label_parametro_2.setVisible(true);
+                label_2pr_0.setVisible(true);
+                slider2.setVisible(true);
+
+                label_parametro_3.setVisible(true);
+                label_3pr_0.setVisible(true);
+                label_3pr_100.setVisible(true);
+                slider3.setVisible(true);
+
+                label_parametro_4.setVisible(true);
+                label_4pr_0.setVisible(true);
+                label_4pr_100.setVisible(true);
+                slider4.setVisible(true);
+
+                label_parametro_5.setVisible(true);
+                label_5pr_0.setVisible(true);
+                label_5pr_100.setVisible(true);
+                slider5.setVisible(true);
+
+                label_parametro_6.setVisible(true);
+                label_6pr_0.setVisible(true);
+                label_6pr_100.setVisible(true);
+                slider6.setVisible(true);
+
+                label_parametro_7.setVisible(false);
+                label_7pr_0.setVisible(false);
+                label_7pr_100.setVisible(false);
+                slider7.setVisible(false);
+
+                boton_convertir.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        label_destino_img.setVisible(true);
+                    }
+                });
 
             }
         });
         item_bajaresolucion.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String ruta_img_blanco_negro= "src/Convertir_Imagenes/img/Costa_Baja_calidad.jpg";
+                ImageIcon img_icon = new ImageIcon(ruta_img_blanco_negro);
+                label_destino_img.setIcon(img_icon);
+                label_destino_img.setVisible(false);
+
+                label_parametro_1.setVisible(true);
+                label_1pr_0.setVisible(true);
+                label_1pr_100.setVisible(true);
+                slider1.setVisible(true);
+
+                label_2pr_100.setVisible(true);
+                label_parametro_2.setVisible(true);
+                label_2pr_0.setVisible(true);
+                slider2.setVisible(true);
+
+                label_parametro_3.setVisible(true);
+                label_3pr_0.setVisible(true);
+                label_3pr_100.setVisible(true);
+                slider3.setVisible(true);
+
+                label_parametro_4.setVisible(true);
+                label_4pr_0.setVisible(true);
+                label_4pr_100.setVisible(true);
+                slider4.setVisible(true);
+
+                label_parametro_5.setVisible(true);
+                label_5pr_0.setVisible(true);
+                label_5pr_100.setVisible(true);
+                slider5.setVisible(true);
+
+                label_parametro_6.setVisible(true);
+                label_6pr_0.setVisible(true);
+                label_6pr_100.setVisible(true);
+                slider6.setVisible(true);
+
+                label_parametro_7.setVisible(true);
+                label_7pr_0.setVisible(true);
+                label_7pr_100.setVisible(true);
+                slider7.setVisible(true);
+
+                boton_convertir.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        label_destino_img.setVisible(true);
+                    }
+                });
 
             }
         });
+
 
 
     }
 
 
     private void abrirImagen() {
+
         JFileChooser fileChooser = new JFileChooser();
+        int returnValue = fileChooser.showOpenDialog(null);
 
-        // Configurar el fileChooser para solo aceptar archivos de imagen
-        fileChooser.setFileFilter(new javax.swing.filechooser.FileFilter() {
-            public boolean accept(File f) {
-                return f.getName().toLowerCase().endsWith(".jpg") ||
-                        f.getName().toLowerCase().endsWith(".jpeg") ||
-                        f.getName().toLowerCase().endsWith(".png") ||
-                        f.isDirectory();
-            }
-
-            public String getDescription() {
-                return "Archivos de imagen (*.jpg, *.jpeg, *.png)";
-            }
-        });
-
-        int result = fileChooser.showOpenDialog(Mainpanel);
-
-        if (result == JFileChooser.APPROVE_OPTION) {
-            File selectedFile = fileChooser.getSelectedFile();
-
-            // Mostrar la imagen en label_origen_img
-            ImageIcon imageIcon = new ImageIcon(selectedFile.getAbsolutePath());
-            label_origen_img.setIcon(imageIcon);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            JOptionPane.showMessageDialog(null, "Imagen seleccionado: " + fileChooser.getSelectedFile().getAbsolutePath());
         }
+
+        String path_img_origen=fileChooser.getSelectedFile().getPath();
+        ImageIcon icon = new ImageIcon(path_img_origen);
+        label_origen_img.setIcon(icon);
+
+
     }
 
 
